b94c188aa6f38614c5ef10cc393241f5d70e76bd
leohintz <ge87soh@mytum.de>
2025-04-26 14:54:03 +0200

    can now customize file name for kml file

--- triangulation/create_kml.py
+++ triangulation/create_kml.py
@@ -2,11 +2,10 @@ import numpy as np
 from decimal import Decimal, getcontext
 from simplekml import Kml
 
-def create_kml(positions: list[tuple[float, float, float]]):
+def create_kml(positions: list[tuple[float, float, float]], filename="drone_flight_track.kml"):
     kml = Kml()
     track = kml.newgxtrack(name="Drone flight Path")
     
     for position in positions:
         track.newgxcoord([position])
-        
-    kml.save("drone_flight_track.kml")
+    kml.save(filename)
