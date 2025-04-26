4413740ac59100602d7b04f8d60057ce3c9b1cef
Simon Morin <simon.morin@web.de>
2025-04-26 12:35:21 +0200

    Reverted merge changes

--- triangulation/triangulate.py
+++ triangulation/triangulate.py
@@ -39,15 +39,3 @@ def triangulate(img1_metadata: DJIMetadata, img2_metadata: DJIMetadata, label_po
     pts3D = pts4D_hom[:3] / pts4D_hom[3]
     return pts3D.ravel()
 
-img_path1 = 'DJI_20250424192954_0007_V.jpeg'
-img_path2 = 'DJI_20250424193048_0052_V.jpeg'
-img_pos1 = (1248, 458)
-img_pos2 = (2132, 1961)
-
-img_1_metadata = read_metadata(img_path1)
-img_2_metadata = read_metadata(img_path2)
-
-result = triangulate(img_1_metadata, img_2_metadata, img_pos1, img_pos2)
-print(result)
-
-
