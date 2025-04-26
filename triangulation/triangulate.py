eb0e7624099b1bd03196ade1791d1ee8d5dbbf1f
Simon Morin <simon.morin@web.de>
2025-04-26 11:41:58 +0200

    Refactored triangulate.py

--- triangulation/triangulate.py
+++ triangulation/triangulate.py
@@ -5,34 +5,34 @@ from metadata import DJIMetadata, read_metadata
 from scipy.spatial.transform import Rotation as R
 from pathlib import Path
 
-def triangulate(img1_metadata: DJIMetadata, img2_metadata: DJIMetadata, label_pos1: tuple[int, int], label_pos2: tuple[int, int]):
-    def compute_camera_matrix(metadata: DJIMetadata):
-        # calculate camera matrix first
-        sensor_width = 9.6
-        sensor_height = 7.2
-        f_0 = metadata.focal_length * metadata.image_width / sensor_width
-        f_1 = metadata.focal_length * metadata.image_height / sensor_height
-        c_0 = metadata.image_width / 2.0
-        c_1 = metadata.image_height / 2.0
-        K = [[f_0, 0.0, c_0],
-            [0.0, f_1, c_1],
-            [0.0, 0, 1.0]]
-        
 
-        # convert gps into x, y, z
-        transformer = Transformer.from_crs("epsg:4326", "epsg:4978")  # WGS84 to ECEF
-        x, y, z = transformer.transform(
-            metadata.latitude, 
-            metadata.longitude, 
-            metadata.absolute_altitude)
-        
-        r = R.from_euler('zyx', [metadata.yaw, metadata.pitch, metadata.roll], degrees=True)
-        R_cam = r.as_matrix()  # 3x3 rotation matrix
-        t = -R_cam @ [x, y, z]
-        Rt = np.hstack((R_cam, t.reshape(3, 1)))
-        P = K @ Rt
-        return P
-    
+def compute_camera_matrix(metadata: DJIMetadata):
+    # calculate camera matrix first
+    sensor_width = 9.6
+    sensor_height = 7.2
+    f_0 = metadata.focal_length * metadata.image_width / sensor_width
+    f_1 = metadata.focal_length * metadata.image_height / sensor_height
+    c_0 = metadata.image_width / 2.0
+    c_1 = metadata.image_height / 2.0
+    K = [[f_0, 0.0, c_0],
+         [0.0, f_1, c_1],
+         [0.0, 0, 1.0]]
+
+    # convert gps into x, y, z
+    transformer = Transformer.from_crs("epsg:4326", "epsg:4978")  # WGS84 to ECEF
+    x, y, z = transformer.transform(
+        metadata.latitude,
+        metadata.longitude,
+        metadata.absolute_altitude)
+
+    r = R.from_euler('zyx', [metadata.yaw, metadata.pitch, metadata.roll], degrees=True)
+    R_cam = r.as_matrix()  # 3x3 rotation matrix
+    t = -R_cam @ [x, y, z]
+    Rt = np.hstack((R_cam, t.reshape(3, 1)))
+    P = K @ Rt
+    return P
+
+def triangulate(img1_metadata: DJIMetadata, img2_metadata: DJIMetadata, label_pos1: tuple[int, int], label_pos2: tuple[int, int]):
     P1 = compute_camera_matrix(img1_metadata)
     P2 = compute_camera_matrix(img2_metadata)
     
